5fe2a8e05298b27ad2926a9ab140c38142c6719b
Sjoerd Hendriks <sjoerd0910@live.com>
2022-05-23 13:09:39 +0200

    ADDED LOGIN FUNCTIONALITY
    Added login and logout pages, connected project to development Auth0 environment, added authentication to home page. Authorsation based on roles does not work yet.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-using Gamification.Data;
+﻿using Gamification.Data;
+using Auth0.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services
+    .AddAuth0WebAppAuthentication(options => {
+        options.Domain = builder.Configuration["Auth0:Domain"];
+        options.ClientId = builder.Configuration["Auth0:ClientId"];
+    });
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
@@ -24,6 +30,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
