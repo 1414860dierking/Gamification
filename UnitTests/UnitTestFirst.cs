839384333d01ce50ea75fe944236f94a544828ea
TimMeijs <timmeijs21@gmail.com>
2022-06-07 13:50:00 +0200

    Tweede unit test door apart bestand aan te maken

---
+++ UnitTests/UnitTestFirst.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Gamification.UnitTests
+{
+    public class UnitTestFirst
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Fail();
+        }
+    }
+}
