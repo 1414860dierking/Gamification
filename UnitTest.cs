a14682fdd4cc44cf41ab9686623e2896cf896931
TimMeijs <timmeijs21@gmail.com>
2022-06-07 15:41:07 +0200

    Unittest toegevoegd

---
+++ UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Gamification
+{
+    public class UnitTest
+    {
+        [Test]
+
+        public void Test()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+    }
+}
