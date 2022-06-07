4d1c330441f628135ed1dddff06e9377413c0423
TimMeijs <timmeijs21@gmail.com>
2022-06-07 12:28:05 +0200

    Simpele testen toegevoegd als class

---
+++ Models/Tests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace Gamification.Models
+{
+    public class Tests
+    {
+        [Test]
+        public void Adding_Funds_Updates_Balance()
+        {
+            Assert.Pass();
+
+            //var account = new BankAccount(1000);
+
+            //account.Add(500);
+
+            //Assert.AreEqual(1500, account.Balance);
+        }
+
+        [Test]
+        public void Adding2_Funds_Updates_Balance()
+        {
+            Assert.Fail();
+
+            //var account = new BankAccount(1000);
+
+            //account.Add(500);
+
+            //Assert.AreEqual(1500, account.Balance);
+        }
+    }
+}
