8037d3ee96f496adac73230859b76e64099af299
Sjoerd Hendriks <sjoerd0910@live.com>
2022-06-15 10:54:30 +0200

    BUGFIXES
    fixed bugs from program.cs by adding missing nuget package

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using Gamification.Data;
-using Gamification.Services;
+﻿using Gamification.Services;
 using Auth0.AspNetCore.Authentication;
 ﻿using Auth0.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Components;
