1f56c1ae174304dc3d883ab5ca53b6219f8a205b
LAPTOP-LT05RHTU\manon <manon@LAPTOP-LT05RHTU>
2022-06-17 12:40:13 +0200

    pagina's toegevoegd voor het aanpassen en maken van de quizen. De frontend werkt volledig. de backend werkt niet!

---
+++ Pages/CreateQuiz.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Gamification.Models;
+using Gamification.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace Gamification.Pages
+{
+    public partial class CreateQuiz
+    {
+
+
+
+        public CreateQuiz()
+        {
+
+        }
+
+
+
+
+
+
+    }
+}
