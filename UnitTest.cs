a8bb4c8a3f1bc51618c92a314a41fe8cf6d3df48
Tim Meijs <timmeijs21@gmail.com>
2022-06-27 09:23:56 +0200

    Laatste werkende UnitTesten update UnitTest.cs

--- UnitTest.cs
+++ UnitTest.cs
@@ -13,140 +13,44 @@ namespace Gamification
     public class UnitTest
     {
         [Test]
-
-        public void Test()
+        public void TestGetQuiz()
         {
-            Assert.Pass();
-        }
+            // Arrange (definiëren van alle benodigde gegevens)
+            IConfiguration configuration = null;
+            ISubjectService subjectService = null;
+            QuizService quizService = new QuizService(configuration, subjectService);
 
-        //[Test]
-        public void Faculties_Show_Be_One()
-        {
-            // AAA
+            Quiz quiz = new Quiz();
+            quiz.Id = 1;
+            quiz.Concept = false;
+            quiz.Name = "Test Quiz";
+            quiz.Description = "Dit is een beschrijving";
+            quiz.OpenToFreePlay = true;
+            quiz.Subject = new Subject();
+            quiz.OpenToKnowledgeBase = true;
+            quiz.Owner = "Test@test.nl";
 
-            // Arrange
-            var expected = 0;
-            var faculties = new List<Faculty>();
-            // Act
-            //faculties = quizService.GetFaculties();
+            quiz.Subject.Id = 1;
+            quiz.Subject.Name = "Subject naam";
+            quiz.Subject.Faculty = new Faculty();
 
-            // Assert
-            Assert.IsTrue(faculties.Count > expected);
-        }
+            quiz.Subject.Faculty.Id = 1;
+            quiz.Subject.Faculty.Name = "ICT";
 
-        [Test]
-
-        public void Test2MustBeFailed()
-        {
-            Assert.Pass();
-        }
-
-        // Het testen van de database waarbij de vragen opgevraagd worden uit de Questions tabel.
-        [Test]
-        public void TestDatabaseQuestionsTable()
-        {
-            //PlayQuiz PlayQuiz = new PlayQuiz();
-
-            //PlayQuiz.GetListOfquestions();
-            //Assert.IsNotNull(PlayQuiz.ListOfQuestions);
-            SqlConnection sqlConnection;
-
-            string sqlconn = "Server=tcp:gamification-dev.database.windows.net,1433;Initial Catalog=Gamification-Dev;Persist Security Info=False;User ID=gamification;Password=rVRm7VSymNs5NUj;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-            sqlConnection = new SqlConnection(sqlconn);
-
-
-            try
-            {
-                sqlConnection.Open();
-
-                List<Question> questions = new List<Question>();
-
-                string queryString = $"SELECT * FROM dbo.Questions";
-                SqlCommand sqlCommand = new SqlCommand(queryString, sqlConnection);
-                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
-
-                while (sqlDataReader.Read())
-                {
-                    questions.Add(new Question()
-                    {
-                        Id = Convert.ToInt32(sqlDataReader["Id"]),
-                        Quiz = null,
-                        Title = sqlDataReader["Title"].ToString(),
-                        Description = sqlDataReader["Description"].ToString(),
-                        TimeToAnswer = Convert.ToInt32(sqlDataReader["TimeToAnswer"]),
-                        SequenceNumber = Convert.ToInt32(sqlDataReader["SequenceNumber"])
-                    }); ;
-                }
-                sqlDataReader.Close();
-                sqlConnection.Close();
-                //questions;
-                Assert.IsNotNull(questions);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        // Het testen van de database waarbij de vragen opgevraagd worden uit de Faculties tabel.
-        [Test]
-        public void TestDatabaseFacultiesTable()
-        {
-            SqlConnection sqlConnection;
-
-            string sqlconn = "Server=tcp:gamification-dev.database.windows.net,1433;Initial Catalog=Gamification-Dev;Persist Security Info=False;User ID=gamification;Password=rVRm7VSymNs5NUj;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-            sqlConnection = new SqlConnection(sqlconn);
-
-            try
-            {
-                sqlConnection.Open();
-
-                List<Faculty> faculties = new List<Faculty>();
-
-                string queryString = $"SELECT * FROM dbo.Faculties";
-                SqlCommand sqlCommand = new SqlCommand(queryString, sqlConnection);
-                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
-
-                while (sqlDataReader.Read())
-                {
-                    faculties.Add(new Faculty()
-                    {
-                        Id = Convert.ToInt32(sqlDataReader["Id"]),
-                        Name = sqlDataReader["Name"].ToString()
-                    });
-                }
-                sqlDataReader.Close();
-                sqlConnection.Close();
-                //questions;
-                Assert.IsNotNull(faculties);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        [Test]
-        public void TestPage()
-        {
-            
-        }
-
-        [Test]
-        public void Test234()
-        {
-            // Arrange
-            using var ctx = new Bunit.TestContext();
-            var cut = ctx.RenderComponent<ClickMe>();
-            var buttonElement = cut.Find("button");
+            Quiz expected = quiz;
 
             // Act
-            buttonElement.Click();
-            buttonElement.Click(detail: 3, ctrlKey: true);
-            buttonElement.Click(new MouseEventArgs());
+            quizService.SetCurrentQuiz(quiz);
+            Quiz CurrentQuiz = quizService.GetCurrentQuiz();
 
             // Assert
-            // ...
+            Assert.AreEqual(CurrentQuiz, expected);
+        }
+
+        [Test]
+        public void TestMustBeFailed()
+        {
+            Assert.Pass();
         }
     }
 }
